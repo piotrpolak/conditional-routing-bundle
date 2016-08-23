3f38e70007360b5ce37e6f986784c484b6750c7c
Piotr Polak <pepis@closbrothers.pl>
2016-08-23 19:12:25 +0200

    Fixed dependency injection conditional_loader.route_resolver support

--- src/DependencyInjection/Compiler/MethodHandlerCompilerPass.php
+++ src/DependencyInjection/Compiler/MethodHandlerCompilerPass.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Reference;
 
 /**
- * Adds tagged conditional_router.routing_loader service support.
+ * Adds tagged conditional_loader.route_resolver service support.
  *
  * @author Piotr Polak <piotr@polak.ro>
  */
@@ -18,15 +18,13 @@ class MethodHandlerCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $key = 'conditional_router.routing_loader';
-
-        if (!$container->has($key)) {
+        if (!$container->has('conditional_router.routing_loader')) {
             return;
         }
 
-        $definition = $container->findDefinition($key);
+        $definition = $container->findDefinition('conditional_router.routing_loader');
 
-        $taggedServicesIds = array_keys($container->findTaggedServiceIds($key));
+        $taggedServicesIds = array_keys($container->findTaggedServiceIds('conditional_loader.route_resolver'));
 
         foreach ($taggedServicesIds as $id) {
             $definition->addMethodCall('addRouteResolver', array(new Reference($id)));
